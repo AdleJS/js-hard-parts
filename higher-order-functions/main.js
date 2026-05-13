69889ab8f1551e331de9cca3b4867828e0281783
AdlePy <askaradilet41@gmail.com>
2026-05-13 19:23:58 +0500

    added higher-oreder-functions section

---
+++ higher-order-functions/main.js
@@ -0,0 +1,40 @@
+function copyArrayAndMultiplyBy2(array) {
+  const output = [];
+
+  for (let i = 0; i < array.length; i++) {
+    output.push(array[i] * 2);
+  }
+
+  return output;
+}
+
+function copyArrayAndDivideBy2(array) {
+  const output = [];
+
+  for (let i = 0; i < array.length; i++) {
+    output.push(array[i] / 2);
+  }
+
+  return output;
+}
+
+function copyArrayAndManipulate(array, instructions) {
+  const output = [];
+  for (let i = 0; i < array.length; i++) {
+    output.push(instructions(array[i]));
+  }
+
+  return output;
+}
+
+function multiplyBy2(input) {
+  return input * 2;
+}
+
+const myArray = [1, 2, 3];
+
+const result = copyArrayAndMultiplyBy2(myArray);
+const anotherResult = copyArrayAndDivideBy2(myArray);
+const yetAnotherResult = copyArrayAndManipulate(myArray, multiplyBy2);
+
+console.log(yetAnotherResult);
