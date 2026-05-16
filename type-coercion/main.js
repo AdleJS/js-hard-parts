654828c042ff17b80ab4fb7db71637f61f6ca4a9
AdlePy <askaradilet41@gmail.com>
2026-05-16 16:42:38 +0500

    updated type-coercion section

--- type-coercion/main.js
+++ type-coercion/main.js
@@ -1,6 +1,17 @@
 // math operators type coercion
 const price = 7;
 let quantity = "3";
-let total = price * quantity;
+let donation = "10";
+const max = 10;
+let total;
 
-console.log(total);
+function onSubmit() {
+  if (quantity) {
+    total = +price * +quantity + +donation;
+    console.log(total);
+  } else {
+    console.log("Add items!");
+  }
+}
+
+onSubmit();
