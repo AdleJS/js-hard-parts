a72471c8821401d4dd6677f29827c98c2b7b1803
AdlePy <askaradilet41@gmail.com>
2026-05-17 18:29:25 +0500

    updated memory section

--- memory/main.js
+++ memory/main.js
@@ -12,7 +12,19 @@ function onSubmit() {
   if (userSubmitted == userStored) {
     console.log("Submit");
   }
+
+  if (+userSubmitted === +userStored) {
+    console.log("primitives are equal");
+  }
 }
+
+function coerce(hint) {
+  if (hint === "string") return "user";
+  if (hint === "number") return 105;
+}
+
+userStored[Symbol.toPrimitive] = coerce;
+userSubmitted[Symbol.toPrimitive] = coerce;
 
 onSubmit();
 
