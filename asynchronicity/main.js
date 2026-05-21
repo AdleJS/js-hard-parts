1b19e5ff288ca5749fbdf78a55743bf0acdf6d24
AdlePy <askaradilet41@gmail.com>
2026-05-21 20:44:16 +0500

    finished asynchronicity section

--- asynchronicity/main.js
+++ asynchronicity/main.js
@@ -2,6 +2,13 @@ function printHello() {
   console.log("Hello");
 }
 
-setTimeout(printHello, 1000);
+function blockForSec() {
+  for (let i = 0; i <= 10000000000; i++) {
+    continue;
+  }
+  console.log("finished");
+}
+setTimeout(printHello, 0);
+blockForSec();
 
 console.log("Me first!");
